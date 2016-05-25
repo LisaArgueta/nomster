d2f86dcd0c9926ac91c5e6daf22b2f33dcc64422
Lisa Argueta <devcub22@gmail.com>
2016-05-25 15:13:21 +0000

    Built the user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Nomster::Application.routes.draw do
     root 'photos#index'
   end
 
-
+  resources :users, only: :show
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
